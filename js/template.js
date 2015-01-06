0e0fdccf0a995f57905421b0b2428cea28cd6d73
emriedel <emriedel@umich.edu>
2015-01-05 19:25:06 -0500

    Updated about page
    New layout for about page- cool pictures and everything

--- js/template.js
+++ js/template.js
@@ -111,5 +111,42 @@
 			});
 		}
 
+		
+	$("iframe#twitter-widget-0").waitUntilExists(function(){
+		$("iframe#twitter-widget-0").contents().find('head').append('<style>img.u-photo.avatar{display:none !important;}.var-chromeless .tweet{padding-left:0;} .footer{display:none !important;} .full-name{display:none !important;} .header{padding-bottom: 5px;} .e-entry-title{font-size: 16px !important;}</style>');
+	});
+		
 	}); // End document ready
 })(this.jQuery);
+
+
+(function ($) {
+ 
+/**
+* @function
+* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+* @param {function} handler A function to execute at the time when the element is inserted
+* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+* @example $(selector).waitUntilExists(function);
+*/
+ 
+$.fn.waitUntilExists	= function (handler, shouldRunHandlerOnce, isChild) {
+	var found	= 'found';
+	var $this	= $(this.selector);
+	var $elements	= $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+	
+	if (!isChild)
+	{
+		(window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+			window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+		;
+	}
+	else if (shouldRunHandlerOnce && $elements.length)
+	{
+		window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+	}
+	
+	return $this;
+}
+ 
+}(jQuery));
